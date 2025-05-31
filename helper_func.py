15cbf45a2c4907084ec619d3dce05b954bfdffea
freaky_coder <168324938+Freaky-coder05@users.noreply.github.com>
2025-05-31 10:50:30 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -3,7 +3,7 @@ import re
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
-from config import FORCE_SUB_CHANNEL, ADMINS, FORCE_SUB_CHANNEL_2
+from config import FORCE_SUB_CHANNEL, ADMINS, FORCE_SUB_CHANNEL_2,FORCE_SUB_CHANNEL_3
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 from database.database import *
@@ -44,24 +44,21 @@ async def is_subscribed2(filter, client, update):
         return True
 
 
-async def is_subscribed3(filter, client, update):    
-    force_sub_channel = get_force_sub_channel()
-    
-    if not force_sub_channel:
-        return True  # No force sub set
-        
+async def is_subscribed3(filter, client, update):
+    if not FORCE_SUB_CHANNEL_3:
+        return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
     try:
-        member = await client.get_chat_member(chat_id = force_sub_channel, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL_3, user_id = user_id)
     except UserNotParticipant:
         return False
 
     if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
         return False
     else:
-        return True
+        return True 
 
 
 async def encode(string):
